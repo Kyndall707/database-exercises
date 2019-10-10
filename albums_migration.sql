e251aa5906e98fd66bcf42a6479cc083817bd16d
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-10 17:24:10 -0500

    finished second ecercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,10 +3,10 @@ drop table if exists albums;
 create table albums
 (
     id           INT(100) unsigned not null auto_increment,
-    artist       VARCHAR(55),
-    name         VARCHAR(50),
-    release_date DATE,
-    sales        FLOAT(5, 2),
-    genre        VARCHAR(20),
+    artist       VARCHAR(55) NOT NULL,
+    name         VARCHAR(100) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales        FLOAT(5, 2) UNSIGNED,
+    genre        VARCHAR(50),
     primary key (id)
 );
