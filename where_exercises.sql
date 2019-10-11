69e01721f5d44bc84f59b37fe18ee742fb329ecd
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-11 12:18:33 -0500

    finished where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -28,3 +28,20 @@ SELECT *
 From employees
 WHERE gender = 'M' AND (first_name = 'Irena' OR 'Vidya' OR 'Maya');
 
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR '%E';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-01-01';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%';
+
+
