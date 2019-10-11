365a0b313cb4caf7fa8266a2d0ded5cf4976022b
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-11 15:24:35 -0500

    finished with the order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -60,3 +60,10 @@ where hire_date like '199%'
   and birth_date like '%12-25'
 order by birth_date, hire_date desc
 ;
+
+
+select *
+from employees
+where last_name like '%q%'
+  and last_name not like '%qu%'
+;
