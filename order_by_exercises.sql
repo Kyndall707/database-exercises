2adf01dcf903dec500fb3ff6a5b8df008f0dafca
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-11 15:22:29 -0500

    almost done with order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -49,3 +49,14 @@ from employees
 where last_name like 'E%'
    or last_name like '%E' order by emp_no;
 
+
+select *
+from employees
+where last_name like 'E%E' order by emp_no desc;
+
+select *
+from employees
+where hire_date like '199%'
+  and birth_date like '%12-25'
+order by birth_date, hire_date desc
+;
