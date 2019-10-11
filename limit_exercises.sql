90c14b6c13ba4d07838a79b7820c095a11c1f757
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-11 16:05:53 -0500

    finished with the input exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,10 @@ select distinct last_name
 from employees
 order by last_name desc
 limit 10;
+
+select *
+from employees
+where hire_date between '1990-01-01' and '2000-01-01'
+  and birth_date like '%%%%-12-25'
+order by birth_date, hire_date desc
+limit 5 offset 45;
