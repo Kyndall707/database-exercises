b35fa2375e21d335425205d59a003091c4393bec
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-15 10:50:08 -0500

    aliases exercise finished

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -4,3 +4,12 @@ SELECT concat(first_name, ", " ,last_name) as full_name
 from employees
 LIMIT 10;
 
+
+SELECT concat(last_name, ", " ,first_name) full_name,  birth_date as DOB
+from employees
+LIMIT 10;
+
+SELECT CONCAT(emp_no, " - " ,last_name, ", " ,first_name) as full_name, birth_date as DOB
+from employees
+limit 10;
+
