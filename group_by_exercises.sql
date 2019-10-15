651ec79c541af4ea99772e9c68444c65a8f2b8cc
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-15 10:55:32 -0500

    finished group-by exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -11,3 +11,14 @@ group by first_name, last_name;
 select distinct last_name
 from employees
 where last_name like '%Q%' AND NOT last_name LIKE '%QU%';
+
+SELECT DISTINCT last_name, COUNT(*)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(*) DESC
+Limit 10;
+
+SELECT COUNT(*) as Count, gender
+from employees
+where first_name in ('Irena','Maya','Vidya')
+group by gender;
