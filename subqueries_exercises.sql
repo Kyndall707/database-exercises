f977673977a5785e2010e6e5838da8cfac245025
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-16 16:07:38 -0500

    half way finished on subqueries exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,2 +1,18 @@
 USE employees;
 
+SELECT *
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+    );
+
+SELECT *
+FROM titles
+WHERE emp_no IN(
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
