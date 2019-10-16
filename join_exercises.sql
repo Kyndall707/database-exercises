69f993f82363bf466781aff4d06d54575e69a5e9
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-16 14:31:06 -0500

    second half finished

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,10 +1,23 @@
 USE employees;
 
-SELECT d.dept_name, concat(e.first_name, ' ', e.last_name) as 'manager'
+SELECT d.dept_name, concat(e.first_name, ' ', e.last_name) AS 'manager'
 FROM employees as e
-         JOIN dept_manager as dm
+         JOIN dept_manager AS dm
               ON dm.emp_no = e.emp_no
          JOIN departments as d
               ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01'
-order by d.dept_name
+ORDER BY d.dept_name;
+
+SELECT d.dept_name, concat(e.first_name, ' ', e.last_name) AS 'manager'
+FROM employees AS e
+    JOIN dept_manager AS dm
+        ON dm.emp_no = e.emp_no
+    JOIN departments AS d
+        ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01'
+    AND e.gender = 'F'
+ORDER BY d.dept_name;
+
+
+
